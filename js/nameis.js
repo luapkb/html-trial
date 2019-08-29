fbbb1da97b47c089b6f797e8eea326eb65087888
Paul Bennett <luapkb@gmail.com>
2019-08-29 16:50:53 -0700

    08290019

--- js/nameis.js
+++ js/nameis.js
@@ -1,3 +1,17 @@
+function welcome (){
+
 var greeting = prompt ('whats your name?');
 
 document.write ('<h1>' + 'Hi, ' + greeting + '  Im Digger' + '</h1>');
+}
+function preference () {
+    var name = prompt ("do you use Mac or Windows?");
+    var answer;
+
+    
+    if (name !=='Mac' && name !=='Windows'){
+        answer = 'Oh, you use that OS. Thats too bad. We can continue anyway.'
+    }
+    
+ return answer   
+}
