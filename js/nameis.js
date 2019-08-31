69375ba2b5eb5ca6bc76bddee4c93c76ed9904dd
Paul Bennett <luapkb@gmail.com>
2019-08-31 11:55:26 -0700

    m

--- js/nameis.js
+++ js/nameis.js
@@ -1,17 +1,37 @@
-function welcome (){
+function welcome() {
 
-var greeting = prompt ('whats your name?');
+    var greeting = prompt('whats your name?');
 
-document.write ('<h1>' + 'Hi, ' + greeting + '  Im Digger' + '</h1>');
+    return ('<h1>' + 'Hi, ' + greeting + '  Im Digger' + '</h1>');
+
 }
-function preference () {
-    var name = prompt ("do you use Mac or Windows?");
+
+function preference() {
+    var name = prompt("do you use Mac or Windows?");
     var answer;
 
-    
-    if (name !=='Mac' && name !=='Windows'){
-        answer = 'Oh, you use that OS. Thats too bad. We can continue anyway.'
+
+    if (name !== 'Mac' && name !== 'Windows') {
+        answer = 'Oh, you use that OS. Well, we can continue anyway.';
     }
-    
- return answer   
+
+    return answer
+}
+
+function askaboutcomputers() {
+    var amount = 'not a number';
+    var computer = '<img src="computer.png">'
+    var computers = '';
+
+    while (isNaN(amount)) {
+        amount = prompt ('how many computers do you have? please enter a number');
+    }
+    while (amount > 5 || (isNaN(amount))) {
+        amount = 'posibly but lets focus aon the number you need help with';
+    }
+    for (var i = 0; i < amount; i++) {
+        computers = computers + computer
+    }
+
+    return computers;
 }
